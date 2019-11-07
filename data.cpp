e3b763563499033b8bfed09aa1996b2731a8eb99
Charlie Haslam <ch17873@my.bristol.ac.uk>
2019-11-07 18:06:39 +0000

    starrt data.cpp

---
+++ data.cpp
@@ -0,0 +1,8 @@
+// header inclusion
+#include <stdio.h>
+#include <stdio.h>
+#include <vector>
+
+//define all the vectors
+int dart4[] = {};
+std::vector<int> dart4 (arr, arr + sizeof(arr) / sizeof(arr[0]) );
